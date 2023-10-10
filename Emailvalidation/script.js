c3a3e0763ff470e6dec48b56588239bbbcc02455
Gokul6373 <139102815+Gokul6373@users.noreply.github.com>
2023-10-10 11:22:39 -0400

    Add files via upload

---
+++ Emailvalidation/script.js
@@ -0,0 +1,17 @@
+// function validate(){
+//     var mail =document.getElementById("text").value;
+
+//     var regx =/^([a-zA-Z0-9\._]+)@([a-zA-Z0-9])+.([a-z]+)(.[a-z]+)?$/
+
+  
+//     if(regx.text(mail)){
+//         alert("You have provided a valid Email Id")
+//         return true
+//     }
+//     else{
+//         alert("Sorry! Incorrect Email Id")
+//         return false;
+//     }
+
+// }
+// /^([a-zA-Z0-9\._])+([a-zA-Z0-9])+.([a-z])+(.[a-z]+)/?$/
